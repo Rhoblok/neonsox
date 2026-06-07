4954fccfce8035c9c79975fddf6c9cae9990e32d
Surkah Xiannine <interstellarinterloperX9@gmail.com>
2026-06-06 21:19:08 -0700

    Create React frontend

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>NEONSOX</h1>
+
+      <p>Slick & Simple Advertisements.</p>
+
+      <button>
+        GET STARTED
+      </button>
+    </div>
+  )
+}
+
+export default App
