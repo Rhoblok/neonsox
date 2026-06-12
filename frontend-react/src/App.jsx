770f28bbfa6aeb4bb2e9111bb890573d6bad3973
Surkah Xiannine <interstellarinterloperX9@gmail.com>
2026-06-11 22:32:18 -0700

    Add custom logo and center tagline

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,15 +1,26 @@
+import './App.css';
+import logoImage from './assets/Picture1.svg';
+
 function App() {
   return (
     <div>
-      <h1>NEONSOX</h1>
-
-      <p>Slick & Simple Advertisements.</p>
-
-      <button>
-        GET STARTED
-      </button>
+      <section className="hero">
+        <div className="logo-block">
+          <div className="logo-row">
+            
+            {/* The tagline is now inside this block, directly under the H1 */}
+            <div className="logo-text-block">
+              <h1 className="logo-neon">NEONSOX</h1>
+              <p className="tagline"> Slick & Simple Advertisements. </p>
+            </div>
+            
+            <img src={logoImage} alt="NEONSOX Logo" className="sock-icon" />
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
